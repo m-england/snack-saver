f243402bf73549fbcac149d361ebfa0435eafe42
m-england <me@markeng.land>
2024-11-02 09:42:55 -0500

    feat: Adding pwa manifest

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://m-england.github.io/snack-saver/sw.js', { scope: 'https://m-england.github.io/snack-saver/' })})}
